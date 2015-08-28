5cd213ae24f452ff7b2169f2fc821e58014fbfe1
diversant <diversant@corp.devoffice.com>
2015-08-28 15:12:13 +0300

    New documentation engine

---
+++ sections/customization-tutorials/en/__description.php
@@ -0,0 +1,2 @@
+<h2 class="item7">Customization Tutorials </h2>
+
---
+++ sections/editing-template/en/editing-html-and-css-files.php
@@ -0,0 +1,12 @@
+<h3>Editing HTML</h3>
+<p>All editable HTML files are located in folders of the template package (index.html).
+
+    To be able to edit the file, you'll need some knowledge of these languages: HTML, CSS. You will also need to use an
+    editor. <b>Notepad</b> (for Windows) is the simplest kind of editor that will open all the above file types.
+    However, it's not convenient for editing, so you'd better get one of the following editors: </p>
+<ul class="marked-list">
+    <li><b>SublimeText</b> <a href="http://www.sublimetext.com/">http://www.sublimetext.com/</a></li>
+    <li><b>Dreamweaver</b> <a href="https://creative.adobe.com/products/dreamweaver">https://creative.adobe.com/products/dreamweaver</a>
+    </li>
+    <li><b>Notepad++</b> <a href="http://notepad-plus-plus.org/">http://notepad-plus-plus.org/</a></li>
+</ul>
